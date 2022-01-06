7ee8a5f87426d17358ab734da7cc0a5e62675db4
ANS <36783010+answebdev@users.noreply.github.com>
2022-01-06 12:27:10 -0800

    udpate server

--- server.js
+++ server.js
@@ -1,41 +1,58 @@
-// const jsonServer = require('json-server');
-// const server = jsonServer.create();
-// const router = jsonServer.router(require('./mock/db.js')());
-// const middlewares = jsonServer.defaults();
-// const port = process.env.PORT || 3000;
-
-// server.use(middlewares);
-// server.use(router);
-
-// console.log('JSON Server is running');
-
-// server.listen(port);
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router(require('./mock/db.js')());
 const middlewares = jsonServer.defaults();
-const express = require('express');
-const bodyParser = require('body-parser');
-const rateLimit = require('express-rate-limit');
-const cors = require('cors');
-const compression = require('compression');
+const { RateLimiter } = require('limiter');
+
 const port = process.env.PORT || 3000;
-
-const app = express();
-app.use(bodyParser.json());
-app.use(cors());
-
-// compress all responses
-app.use(compression());
 
 // server.use(middlewares);
 // server.use(router);
-app.use(middlewares);
-app.use(router);
 
-app.use(router);
+// Allow 150 requests per hour (the Twitter search limit). Also understands
+// 'second', 'minute', 'day', or a number of milliseconds
+const limiter = new RateLimiter({ tokensPerInterval: 3, interval: 'hour' });
 
-app.set('trust proxy', 1);
+async function sendRequest() {
+  // This call will throw if we request more than the maximum number of requests
+  // that were set in the constructor
+  // remainingRequests tells us how many additional requests could be sent
+  // right this moment
+  const remainingRequests = await limiter.removeTokens(1);
+  callMyRequestSendingFunction();
+}
+
+console.log('JSON Server is running');
+
+server.listen(port);
+
+// =======================================================
+// const jsonServer = require('json-server');
+// const server = jsonServer.create();
+// const router = jsonServer.router(require('./mock/db.js')());
+// const middlewares = jsonServer.defaults();
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const rateLimit = require('express-rate-limit');
+// const cors = require('cors');
+// const compression = require('compression');
+// const port = process.env.PORT || 3000;
+
+// const app = express();
+// app.use(bodyParser.json());
+// app.use(cors());
+
+// // compress all responses
+// app.use(compression());
+
+// // server.use(middlewares);
+// // server.use(router);
+// app.use(middlewares);
+// app.use(router);
+
+// app.use(router);
+
+// app.set('trust proxy', 1);
 
 // const limiter = rateLimit({
 //   windowMs: 15 * 60 * 1000, // 15 minutes
@@ -44,13 +61,6 @@ app.set('trust proxy', 1);
 
 // app.use(limiter);
 
-const limiter = rateLimit({
-  max: 3,
-  windowMs: 60 * 60 * 1000,
-  message: 'too many requests sent by this ip, please try again in an hour !',
-});
+// // server.listen(port, () => console.log(`Server running on port ${port}`));
 
-app.use('https://personal-music-api.herokuapp.com/artists', limiter); // eg: app.use('/api', limiter);
-// server.listen(port, () => console.log(`Server running on port ${port}`));
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
+// app.listen(port, () => console.log(`Server running on port ${port}`));
