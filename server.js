342b69698050428b7d09c30b9975999c4ad96b6c
ANS <36783010+answebdev@users.noreply.github.com>
2022-01-06 15:16:06 -0800

    update server

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
-// app.set('trust proxy', 1);
+app.set('trust proxy', 1);
 
 // Put before your path
 const limiter = rateLimit({
@@ -39,11 +39,12 @@ const limiter = rateLimit({
 server.use(middlewares);
 server.use(router);
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './mock/db.js'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, './mock/db.js'));
+// });
 
 // app.get('/', limiter, (req, res) => res.send('Hello'));
+app.get('*', limiter, (req, res) => res.send('./mock/db.js'));
 
 console.log('JSON Server is running');
 
