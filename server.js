62c4161553e699d1f2d29533a53d45f3f6c8a759
ANS <36783010+answebdev@users.noreply.github.com>
2022-01-06 15:04:58 -0800

    update server

--- server.js
+++ server.js
@@ -34,12 +34,12 @@ const limiter = rateLimit({
 // Apply to all requests
 app.use(limiter);
 
+server.use(middlewares);
+server.use(router);
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './mock/db.js'));
 });
-
-server.use(middlewares);
-server.use(router);
 
 console.log('JSON Server is running');
 
