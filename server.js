fc3e149ffd6961bd988e2e9f425932201cd8778b
ANS <36783010+answebdev@users.noreply.github.com>
2022-01-06 12:36:13 -0800

    udpate server

--- server.js
+++ server.js
@@ -1,69 +1,51 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router(require('./mock/db.js')());
-const middlewares = jsonServer.defaults();
-const { RateLimiter } = require('limiter');
-
-const port = process.env.PORT || 3000;
-
-// server.use(middlewares);
-// server.use(router);
-
-// Allow 150 requests per hour (the Twitter search limit). Also understands
-// 'second', 'minute', 'day', or a number of milliseconds
-const limiter = new RateLimiter({ tokensPerInterval: 3, interval: 'hour' });
-
-async function sendRequest() {
-  // This call will throw if we request more than the maximum number of requests
-  // that were set in the constructor
-  // remainingRequests tells us how many additional requests could be sent
-  // right this moment
-  const remainingRequests = await limiter.removeTokens(1);
-  callMyRequestSendingFunction(() => {
-    server.use(middlewares);
-    server.use(router);
-  });
-}
-
-console.log('JSON Server is running');
-
-server.listen(port);
-
-// =======================================================
 // const jsonServer = require('json-server');
 // const server = jsonServer.create();
 // const router = jsonServer.router(require('./mock/db.js')());
 // const middlewares = jsonServer.defaults();
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const rateLimit = require('express-rate-limit');
-// const cors = require('cors');
-// const compression = require('compression');
 // const port = process.env.PORT || 3000;
 
-// const app = express();
-// app.use(bodyParser.json());
-// app.use(cors());
+// server.use(middlewares);
+// server.use(router);
 
-// // compress all responses
-// app.use(compression());
+// console.log('JSON Server is running');
 
-// // server.use(middlewares);
-// // server.use(router);
-// app.use(middlewares);
-// app.use(router);
+// server.listen(port);
 
-// app.use(router);
+// =======================================================
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router(require('./mock/db.js')());
+const middlewares = jsonServer.defaults();
+const express = require('express');
+const bodyParser = require('body-parser');
+const rateLimit = require('express-rate-limit');
+const cors = require('cors');
+const compression = require('compression');
+const port = process.env.PORT || 3000;
 
-// app.set('trust proxy', 1);
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 
-// const limiter = rateLimit({
-//   windowMs: 15 * 60 * 1000, // 15 minutes
-//   max: 3,
-// });
+// compress all responses
+app.use(compression());
 
-// app.use(limiter);
+// server.use(middlewares);
+// server.use(router);
+app.use(middlewares);
+app.use(router);
 
-// // server.listen(port, () => console.log(`Server running on port ${port}`));
+app.use(router);
 
-// app.listen(port, () => console.log(`Server running on port ${port}`));
+app.set('trust proxy', 1);
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 3,
+});
+
+app.use(limiter);
+
+// server.listen(port, () => console.log(`Server running on port ${port}`));
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
