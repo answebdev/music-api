0ac53c66c6e94063272a0442084c1cd522aa041d
ANS <36783010+answebdev@users.noreply.github.com>
2022-01-06 12:01:30 -0800

    udpate server

--- server.js
+++ server.js
@@ -10,32 +10,32 @@
 // console.log('JSON Server is running');
 
 // server.listen(port);
-// const jsonServer = require('json-server');
-// const server = jsonServer.create();
-// const router = jsonServer.router(require('./mock/db.js')());
-// const middlewares = jsonServer.defaults();
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const rateLimit = require('express-rate-limit');
-// const cors = require('cors');
-// const compression = require('compression');
-// const port = process.env.PORT || 3000;
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router(require('./mock/db.js')());
+const middlewares = jsonServer.defaults();
+const express = require('express');
+const bodyParser = require('body-parser');
+const rateLimit = require('express-rate-limit');
+const cors = require('cors');
+const compression = require('compression');
+const port = process.env.PORT || 3000;
 
-// const app = express();
-// app.use(bodyParser.json());
-// app.use(cors());
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 
-// // compress all responses
-// app.use(compression());
+// compress all responses
+app.use(compression());
 
-// // server.use(middlewares);
-// // server.use(router);
-// app.use(middlewares);
-// app.use(router);
+// server.use(middlewares);
+// server.use(router);
+app.use(middlewares);
+app.use(router);
 
-// app.use(router);
+app.use(router);
 
-// app.set('trust proxy', 1);
+app.set('trust proxy', 1);
 
 // const limiter = rateLimit({
 //   windowMs: 15 * 60 * 1000, // 15 minutes
@@ -43,16 +43,6 @@
 // });
 
 // app.use(limiter);
-
-// // server.listen(port, () => console.log(`Server running on port ${port}`));
-
-// app.listen(port, () => console.log(`Server running on port ${port}`));
-
-const express = require('express');
-const rateLimit = require('express-rate-limit');
-const app = express();
-
-const PORT = 3000;
 
 // Create the rate limit rule
 const apiRequestLimiter = rateLimit({
@@ -63,10 +53,14 @@ const apiRequestLimiter = rateLimit({
 // Use the limit rule as an application middleware
 app.use(apiRequestLimiter);
 
-app.get('./mock/db.js', function (req, res) {
+app.get('http://personal-music-api.herokuapp.com/', function (req, res) {
   return res.send('Hello World');
 });
 
-app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`server started on port ${port}`);
 });
+
+// server.listen(port, () => console.log(`Server running on port ${port}`));
+
+// app.listen(port, () => console.log(`Server running on port ${port}`));
