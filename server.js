d7c8db22999e1cf2cd7246bdf34f6cbc4f0b270c
ANS <36783010+answebdev@users.noreply.github.com>
2022-01-06 12:48:21 -0800

    udpate server

--- server.js
+++ server.js
@@ -1,59 +1,12 @@
-// const jsonServer = require('json-server');
-// const server = jsonServer.create();
-// const router = jsonServer.router(require('./mock/db.js')());
-// const middlewares = jsonServer.defaults();
-// const port = process.env.PORT || 3000;
-
-// server.use(middlewares);
-// server.use(router);
-
-// console.log('JSON Server is running');
-
-// server.listen(port);
-
-// =======================================================
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router(require('./mock/db.js')());
 const middlewares = jsonServer.defaults();
-const express = require('express');
-const bodyParser = require('body-parser');
-const rateLimit = require('express-rate-limit');
-const cors = require('cors');
-const compression = require('compression');
 const port = process.env.PORT || 3000;
 
-const app = express();
-app.use(bodyParser.json());
-app.use(cors());
+server.use(middlewares);
+server.use(router);
 
-// compress all responses
-app.use(compression());
+console.log('JSON Server is running');
 
-// server.use(middlewares);
-// server.use(router);
-app.use(middlewares);
-app.use(router);
-
-app.use(router);
-
-app.set('trust proxy', 1);
-
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 3,
-});
-
-app.use(limiter);
-
-// GET route to handle the request coming from user
-app.get(router, (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    message: 'Hello from the express server',
-  });
-});
-
-// server.listen(port, () => console.log(`Server running on port ${port}`));
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
+server.listen(port);
