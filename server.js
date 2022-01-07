8801a34ff64181f0ded975700f6284f0631fc4d1
ANS <36783010+answebdev@users.noreply.github.com>
2022-01-06 17:19:47 -0800

    update server

--- server.js
+++ server.js
@@ -1,48 +1,48 @@
 // ORIGINAL WORKING CODE
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router(require('./mock/db.js')());
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
-
-server.use(middlewares);
-server.use(router);
-
-console.log('JSON Server is running');
-
-server.listen(port);
-
-// =================================================================
-
-// const express = require('express');
-// const rateLimit = require('express-rate-limit');
-// const res = require('express/lib/response');
 // const jsonServer = require('json-server');
 // const server = jsonServer.create();
 // const router = jsonServer.router(require('./mock/db.js')());
 // const middlewares = jsonServer.defaults();
-// const path = require('path');
 // const port = process.env.PORT || 3000;
-
-// const app = express();
-
-// app.set('trust proxy', 1);
-
-// // Put before your path
-// const limiter = rateLimit({
-//   // windowMs: 15 * 60 * 1000, // 15 minutes
-//   windowMs: 1000, // 15 minutes
-//   max: 1,
-//   message: 'Do NOT SEND so many messages again...it is too many',
-// });
-
-// // Apply to all requests
-// server.use(limiter);
 
 // server.use(middlewares);
 // server.use(router);
 
 // console.log('JSON Server is running');
 
-// // app.listen(port);
 // server.listen(port);
+
+// =================================================================
+
+const express = require('express');
+const rateLimit = require('express-rate-limit');
+const res = require('express/lib/response');
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router(require('./mock/db.js')());
+const middlewares = jsonServer.defaults();
+const path = require('path');
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.set('trust proxy', 1);
+
+// Put before your path
+const limiter = rateLimit({
+  // windowMs: 15 * 60 * 1000, // 15 minutes
+  windowMs: 1000, // 15 minutes
+  max: 1,
+  message: 'Do NOT SEND so many messages again...it is too many',
+});
+
+// Apply to all requests
+server.use(limiter);
+
+server.use(middlewares);
+server.use(router);
+
+console.log('JSON Server is running');
+
+// app.listen(port);
+server.listen(port);
